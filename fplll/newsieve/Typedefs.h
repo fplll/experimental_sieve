87058e1f5a0a976e73ff40d6efa1ac00991b65f7
Gottfried Herold <gottfried.herold@rub.de>
2017-07-19 15:30:58 +0200

    Typedefs.h style-checked.

--- fplll/newsieve/Typedefs.h
+++ fplll/newsieve/Typedefs.h
@@ -1,38 +1,41 @@
+// This header defines (global) traits. We might want to encapsulte these in some trait classes that
+// get forwarded, eventually.
+// This header should not depend on anything.
+
+// Consider renaming the file to avoid clashes.
+
+#ifndef GAUSS_SIEVE_TYPEDEFS_H
+#define GAUSS_SIEVE_TYPEDEFS_H
+
+namespace GaussSieve
+{
+// unfortunately, trigonometric functions to compute pi don't have constexpr variants on all
+// compilers we want to support, so we just define pi directly
+long double constexpr pi_long = 3.14159265358979323846264338327950288419716939937510L;
+double constexpr pi_double    = 3.14159265358979323846264338327950288419716939937510;
+long double constexpr pi      = 3.14159265358979323846264338327950288419716939937510L;
+};
+
+// forward-declarations:
+template <class ET, int nfixed> class MyLatticePoint;
+
+// various typedef declarations that control the types used by our classes.
+
+// lines are too long, clang-format destroys vertical alignment
 // clang-format off
+namespace GaussSieve
+{
+template <class ET, bool MT, int nfixed> using GaussSampler_ReturnType = MyLatticePoint<ET, nfixed>;
 
-#ifndef TYPEDEFS_H
-#define TYPEDEFS_H
+template <class ET, bool MT, int nfixed> using GaussList_ReturnType    = MyLatticePoint<ET, nfixed>;
+template <class ET, bool MT, int nfixed> using GaussList_StoredPoint   = MyLatticePoint<ET, nfixed>;
 
-#include "LatticePointsNew.h"
-#include "MyLatticePointClass.cpp"
+template <class ET, bool MT, int nfixed> using GaussQueue_ReturnType   = GaussSampler_ReturnType<ET, MT, nfixed>;
+template <class ET, bool MT, int nfixed> using GaussQueue_DataType     = GaussQueue_ReturnType<ET, MT, nfixed>;
 
-//various typedef declarations that control the types used by our classes.
-//This typedef defines the return type that the Sampler should have.
-
-namespace GaussSieve{
-// unfortunately, trigonometric functions to compute pi don't have constexpr variants on all compilers we want to support, so we just define pi directly
-long double constexpr   pi_long     = 3.14159265358979323846264338327950288419716939937510L;
-double constexpr        pi_double   = 3.14159265358979323846264338327950288419716939937510;
-long double constexpr   pi          = 3.14159265358979323846264338327950288419716939937510L;
+// for a small number of lattice points that we need to access very often.
+template <class ET, bool MT, int nfixed> using FastAccess_Point = MyLatticePoint<ET, nfixed>;
 };
-
-
-namespace GaussSieve{
-
-    template<class ET,bool MT, int nfixed> using GaussSampler_ReturnType = MyLatticePoint<ET,nfixed>;
-
-    template<class ET,bool MT, int nfixed> using GaussList_ReturnType = MyLatticePoint<ET, nfixed>;
-    template<class ET,bool MT, int nfixed> using GaussList_StoredPoint = MyLatticePoint<ET,nfixed>;
-    //template<class ET,bool MT, int nfixed> using GaussList_DereferencesTo = ApproximateLatticePoint<ET,nfixed>;
-
-    template<class ET,bool MT, int nfixed> using GaussQueue_ReturnType = GaussSampler_ReturnType<ET,MT,nfixed>;
-    template<class ET,bool MT, int nfixed> using GaussQueue_DataType   = GaussQueue_ReturnType<ET,MT,nfixed>;
-
-    template<class ET,bool MT, int nfixed> using FastAccess_Point      = MyLatticePoint<ET, nfixed>; //for a small number of lattice points that we need to access very often.
-
-};
-
+// clang-format on
 
 #endif
-
-//clang-format on
