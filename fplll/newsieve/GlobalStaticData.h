f25cfa1778ba38b371b33f54b068d548f267f839
Gottfried Herold <gottfried.herold@rub.de>
2017-10-16 11:07:10 +0200

    Improve documentation.

--- fplll/newsieve/GlobalStaticData.h
+++ fplll/newsieve/GlobalStaticData.h
@@ -12,19 +12,21 @@ namespace GaussSieve{
 CREATE_MEMBER_TYPEDEF_CHECK_CLASS(StaticInitializerArgTag,IsArgForStaticInitializer);
 CREATE_MEMBER_TYPEDEF_CHECK_CLASS(HasDefaultStaticInitializer,IsStaticInitializerDefaulted);
 
-//
-
+// forward declarations:
 template<class T> class StaticInitializer;
 template<class T> class DefaultStaticInitializer;
 template<class DimensionType> struct StaticInitializerArg;
 
-
-// This is the default initializer, which does nothing apart from counting number of instances.
-// Note that typically, we inherit from this, which is the reason why it is templated by T.
+/**
+  This is the default initializer, which does nothing apart from counting number of instances.
+  Note that typically, we inherit from this, which is the reason why it is templated by T.
+  Usage: This class shall only be used by StaticInitializer<T>
+*/
 
 template<class T>
 class DefaultStaticInitializer
 {
+  friend StaticInitializer<T>;
   public:
 #ifndef DEBUG_SIEVE_LP_INIT
   static bool constexpr is_initialized() { return true; } // may be overloaded
@@ -33,6 +35,7 @@ class DefaultStaticInitializer
 #endif
   static unsigned int get_user_count() { return user_count; }
   static unsigned int user_count; // counts the number of objects of this type that exist, essentially.
+  private:
   explicit DefaultStaticInitializer(){ ++user_count; };
 
   template<class X,TEMPL_RESTRICT_DECL(IsArgForStaticInitializer<X>::value)>
@@ -52,6 +55,25 @@ class DefaultStaticInitializer
 // initialize static data this class:
 template<class T> unsigned int DefaultStaticInitializer<T>::user_count = 0;
 
+/**
+  StaticInitializer is a RAII class that manages static initialization of Data.
+  Usage:
+  Create an object StaticInitializer<T> init_T(args);
+  During the livetime of the object init_T, the class T is considered initialized.
+
+  The exact implementation of StaticInitializer<T> depends on T.
+  The implementation below is a default implementation that essentially does nothing.
+  It may be specialized for certain T's.
+  Safeguard: In order to enable the default implementation, the class T has to declare a
+  public member typedef HasDefaultStaticInitializer, e.g.
+  using HasDefaultStaticInitializer = std::true_type; otherwise, you get a compilation error.
+
+  The type of args may be any integral class (deprecated) or
+  any class that has a public typedef StaticInitializerArgTag.
+  It is the job of the the specialization to ensure that arg has the required data.
+  SieveTraits has a GlobalStaticData class supposed to be usable for all such args.
+*/
+
 // StaticInitializer<T> for classes T that have the IsStaticInitializerDefaulted Trait
 template<class T>
 class StaticInitializer : public DefaultStaticInitializer<T>
@@ -67,6 +89,12 @@ class StaticInitializer : public DefaultStaticInitializer<T>
 };
 
 
+
+/**
+  StaticInitializerArg<DimensionType> encapsulates an argument of type
+  DimensionType.
+  It is meant to be an argument for static initializers.
+*/
 
 template<class DimensionType>
 struct StaticInitializerArg
