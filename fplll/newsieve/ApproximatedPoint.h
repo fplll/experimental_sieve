5b0666a7a84db133e22092dd499a9de60430fdd9
Gottfried Herold <gottfried.herold@rub.de>
2017-10-25 12:21:12 +0200

    Restored ApproximatedPoint.h

--- fplll/newsieve/ApproximatedPoint.h
+++ fplll/newsieve/ApproximatedPoint.h
@@ -190,90 +190,90 @@ class VectorWithApproximation
   constexpr      ApproxType const & access_approx() const { return approx; }
   CPP14CONSTEXPR ApproxType       & access_approx()       { return approx; }
 
-
-
-//
-//  static std::string class_name() { return ELP::class_name() + "with approximation"; } // TODO:class_name for Approximation
+  static std::string class_name() { return ELP::class_name() + "with approximation"; } // TODO:class_name for Approximation
 //
 //  // operators<,>,<=, >= : No overloads. Defaults to exact comparison.
 //
 //  // forward [] to exact class
-//  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesCoos<T>)>
-//  ExactCoos &operator[](Arg &&arg) { return exact_point[std::forward<Arg>(arg)]; }
-//  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesCoos<T>)>
-//  ExactCoos const &operator[](Arg &&arg) const { return exact_point[std::forward<Arg>(arg)]; }
+  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesCoos<T>)>
+  ExactCoos &operator[](Arg &&arg) { return exact_point[std::forward<Arg>(arg)]; }
+  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesCoos<T>)>
+  ExactCoos const &operator[](Arg &&arg) const { return exact_point[std::forward<Arg>(arg)]; }
 //
 //  // +=, -=, *= and unary- just forward to the exact class and recompute the approximation.
-//  template<class LatP2, TEMPL_RESTRICT_DECL2(IsALatticePoint<LatP2>)>
-//  VectorWithApproximation& operator+=(LatP2 const &x2) { exact_point+=x2; recompute_approx(); return *this; }
+  template<class LatP2, TEMPL_RESTRICT_DECL2(IsALatticePoint<LatP2>)>
+  VectorWithApproximation& operator+=(LatP2 const &x2) { exact_point+=x2; recompute_approx(); return *this; }
 //
-//  template<class LatP2, TEMPL_RESTRICT_DECL2(IsALatticePoint<LatP2>)>
-//  VectorWithApproximation& operator-=(LatP2 const &x2) { exact_point-=x2; recompute_approx(); return *this; }
+  template<class LatP2, TEMPL_RESTRICT_DECL2(IsALatticePoint<LatP2>)>
+  VectorWithApproximation& operator-=(LatP2 const &x2) { exact_point-=x2; recompute_approx(); return *this; }
 //
-//  template<class Integer>
-//  VectorWithApproximation& operator*=(Integer &&x2) { exact_point*=std::forward<Integer>(x2); recompute_approx(); return *this; }
+  template<class Multiplier>
+  VectorWithApproximation& operator*=(Multiplier &&x2) { exact_point*=std::forward<Multiplier>(x2); recompute_approx(); return *this; }
 //
 //  // TODO: Inefficient
-//  VectorWithApproximation operator-()&&
-//  {
-//    return static_cast<VectorWithApproximation>(-std::move(exact_point));
-//  }
+  VectorWithApproximation operator-()&&
+  {
+    return static_cast<VectorWithApproximation>(-std::move(exact_point));
+  }
 //
 //  // equality comparison.
-//  template<class LatP2, TEMPL_RESTRICT_DECL2(IsALatticePoint<LatP2>)>
-//  bool operator==(LatP2 const &x2) const { return exact_point == x2;};
-//  bool operator==(VectorWithApproximation const &x2) const
-//  {
-//    if(approx!=x2.approx)
-//    {
-//      return false;
-//    }
-//    else
-//    {
-//      return exact_point == x2.exact_point;
-//    }
-//  }
+  template<class LatP2, TEMPL_RESTRICT_DECL2(IsALatticePoint<LatP2>)>
+  bool operator==(LatP2 const &x2) const { return exact_point == x2;};
+  bool operator==(VectorWithApproximation const &x2) const
+  {
+    if(approx!=x2.approx)
+    {
+      return false;
+    }
+    else
+    {
+      return exact_point == x2.exact_point;
+    }
+  }
 //
 //  // forward internal_rep to exact point if it exists.
-//  template<class T=ELP, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<T>)>
-//  auto get_internal_rep_size() const -> decltype( std::declval<ELP>().get_internal_rep_size() ) { return exact_point.get_internal_rep_size(); }
-//  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<T>)>
-//  RepCooType const & get_internal_rep(Arg &&arg) const { return exact_point.get_internal_rep(std::forward<Arg>(arg)); }
-//  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<T>, Has_InternalRep_RW<T>)>
-//  RepCooType & get_internal_rep(Arg &&arg) {return exact_point.get_internal_rep(std::forward<Arg>(arg));}
+  template<class T=ELP, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<T>)>
+  auto get_internal_rep_size() const -> decltype( std::declval<ELP>().get_internal_rep_size() ) { return exact_point.get_internal_rep_size(); }
+  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<T>)>
+  RepCooType const & get_internal_rep(Arg &&arg) const { return exact_point.get_internal_rep(std::forward<Arg>(arg)); }
+  template<class T=ELP, class Arg, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<T>, Has_InternalRep_RW<T>)>
+  RepCooType & get_internal_rep(Arg &&arg) {return exact_point.get_internal_rep(std::forward<Arg>(arg));}
 //
 //  // forward absolute coos to exact point
-//  template<class Arg>
-//  AbsoluteCooType get_absolute_coo(Arg &&arg) const { return exact_point.get_absolute_coo(std::forward<Arg>(arg));  }
+  template<class Arg>
+  AbsoluteCooType get_absolute_coo(Arg &&arg) const { return exact_point.get_absolute_coo(std::forward<Arg>(arg));  }
 //
 //  // forward get_dim
-//  auto get_dim() const -> decltype( std::declval<ELP>().get_dim() ) { return exact_point.get_dim(); }
+  auto get_dim() const -> decltype( std::declval<ELP>().get_dim() ) { return exact_point.get_dim(); }
 //
 //
-//  inline std::ostream& write_lp_to_stream(std::ostream &os, bool const include_norm2=true, bool const include_approx =true) const
-//  {
-//    exact_point.write_lp_to_stream(os, include_norm2,include_approx);
-//    if(include_approx)
-//    {
-//      os << approx;
-//    }
-//    return os;
-//  }
+  inline std::ostream& write_lp_to_stream(std::ostream &os, bool const include_norm2=true, bool const include_approx =true) const
+  {
+    exact_point.write_lp_to_stream(os, include_norm2,include_approx);
+    if(include_approx)
+    {
+      os << approx;
+    }
+    return os;
+  }
 //
-//  template<class T=ELP, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<ELP>)>
-//  inline std::ostream& write_lp_rep_to_stream(std::ostream &os) const { return exact_point.write_lp_rep_to_stream(os); }
+  template<class T=ELP, TEMPL_RESTRICT_DECL2(Has_ExposesInternalRep<ELP>)>
+  inline std::ostream& write_lp_rep_to_stream(std::ostream &os) const { return exact_point.write_lp_rep_to_stream(os); }
 //
 //
 //  //TODO: read_from_stream
 //
-//  void fill_with_zero() { exact_point.fill_with_zero(); recompute_approx(); }
-//  void make_negative()  { exact_point.make_negative(); recompute_approx(); } // todo : may optimize
-//  bool is_zero() { return exact_point.is_zero(); }
-//  VectorWithApproximation make_copy() const { return VectorWithApproximation(exact_point.make_copy()); }
-//
-//  void sanitize() { exact_point.sanitize(); recompute_approx(); }
-//  void sanitize(ExactScalarProductType const &norm2) { exact_point.sanitize(norm2); recompute_approx(); }
-//  void recompute_approx() { approx = static_cast<Approximation>(exact_point); }
+  void fill_with_zero() { exact_point.fill_with_zero(); recompute_approx(); }
+  void make_negative()  { exact_point.make_negative(); recompute_approx(); } // todo : may optimize
+  bool is_zero() { return exact_point.is_zero(); }
+
+  // TODO: Copy approximation
+  VectorWithApproximation make_copy() const { return VectorWithApproximation(exact_point.make_copy()); }
+
+// TODO: More efficient sanitize routines that take prior knowledge into account.
+  void sanitize() { exact_point.sanitize(); recompute_approx(); }
+  void sanitize(ExactScalarProductType const &norm2) { exact_point.sanitize(norm2); recompute_approx(); }
+  void recompute_approx() { approx = static_cast<Approximation>(exact_point); }
 //
 //  inline DelayedNorm2Type get_norm2() const
 //  {
