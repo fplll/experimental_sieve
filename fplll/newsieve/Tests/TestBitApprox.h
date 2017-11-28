d9726d363609a9154f18adb71f427ff6334db7b7
Gottfried Herold <gottfried.herold@rub.de>
2017-11-28 18:45:48 +0100

    Turned some loop variables to unsigned to silence compiler warnings.

--- fplll/newsieve/Tests/TestBitApprox.h
+++ fplll/newsieve/Tests/TestBitApprox.h
@@ -16,10 +16,10 @@ bool test_bit_approx()
 {
 
   int constexpr dim = 67;
-  
+
   typedef GaussSieve::ExactLatticePoint<long, -1> LP;
   using GaussSieve::MaybeFixed;
-  
+
   std::array<long,dim> arr;
   std::array<long,dim> arr2;
   for(int i=0;i<dim;++i)
@@ -27,48 +27,48 @@ bool test_bit_approx()
     arr[i] = std::pow(-1, i+1) * i;
     arr2[i] = std::pow(-1, i) * (i-1)+13;
   }
-  
+
   GaussSieve::StaticInitializer<LP> init1 (MaybeFixed<-1>{dim});
   LP latp = GaussSieve::make_from_any_vector<LP>(arr,MaybeFixed<-1>{dim});
   LP latp2 = GaussSieve::make_from_any_vector<LP>(arr2,MaybeFixed<-1>{dim});
-  
+
   std::cout << "latp = " << latp << std::endl;
   std::cout << "latp2 = " << latp2 << std::endl;
-  
-  
-  for (int i = 0; i<GaussSieve::SimHash::num_of_levels; ++i)
+
+
+  for (unsigned int i = 0; i<GaussSieve::SimHash::num_of_levels; ++i)
   {
     GaussSieve::SimHash::BitApproxScalarProduct approx_scprod_res = compute_sc_product_bitapprox_level(latp, latp2, i);
     std::cout << static_cast<uint_fast32_t>(approx_scprod_res) <<std::endl;
   }
-  
+
   int constexpr dim_var = 70;
   std::array<long,dim_var> arr3;
-  
-  for(int i=0;i<dim_var;++i)
+
+  for(unsigned int i=0;i<dim_var;++i)
   {
     arr3[i] = std::pow(-1, i+1) * i - (i/2);
   }
-  
+
   //typedef GaussSieve::ExactLatticePoint<long, 70> LPFixed;
   //GaussSieve::StaticInitializer<LPFixed> init2 (MaybeFixed<70>{dim_var});
-  
+
   //LPFixed latp3 = GaussSieve::make_from_any_vector<LPFixed>(arr3,MaybeFixed<70>{dim_var});
   //std::cout << "latp3 = " << latp3 << std::endl;
-  
-  
-  
-  
+
+
+
+
   //LP latp2 = GaussSieve::make_from_any_vector<LP>(arr2,MaybeFixed<dim>{dim});
-  
+
   //std::cout << "exact point with approximations: " <<  latp << std::endl;
 
   /*
- 
+
   int constexpr dimfixed=20;
 
- 
-  
+
+
   using GaussSieve::BitApproximation;
 
 
