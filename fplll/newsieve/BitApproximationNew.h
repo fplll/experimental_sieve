e44c368c4a18c7b16704915cd8671b5bfb1183b2
Gottfried Herold <gottfried.herold@rub.de>
2017-12-09 02:17:11 +0100

    Slightly improved WH-Trafo. Should not matter for speed anymore.

--- fplll/newsieve/BitApproximationNew.h
+++ fplll/newsieve/BitApproximationNew.h
@@ -98,7 +98,7 @@ template<std::size_t sim_hash_len_arg, std::size_t sim_hash_num_arg, bool MT, cl
 class CoordinateSelection
 {
 public:
-  static unsigned int constexpr num_of_transforms = 100;  // affects the "quality" vs. speed tradeoff
+  static unsigned int constexpr num_of_transforms = 3;  // affects the "quality" vs. speed tradeoff
   static std::size_t constexpr sim_hash_num = sim_hash_num_arg;
   static std::size_t constexpr sim_hash_len = sim_hash_len_arg;
   static_assert(is_a_power_of_two(sim_hash_len),"");
@@ -125,11 +125,15 @@ private:
   auto inline fast_partial_walsh_hadamard(std::vector<T> input) const
     -> std::vector<T>;
 
+  // performs WH-Trafo (well, almost: up to a fixed permutation / sign-flips) in
+  // the first fast_walsh_hadamard_len coordinates. Modifies the argument (hence inplace).
+  // This version is an order of magnitude faster than the above.
   template<class T>
-  void inline faster_partial_walsh_hadamard(std::vector<T> & input) const;
+  void inline faster_almost_partial_walsh_hadamard_inplace(std::vector<T> & input) const;
 
   unsigned int number_of_blocks;  // TODO: rename
   unsigned int fast_walsh_hadamard_len;
+  unsigned int fast_walsh_hadamard_loglen;
   std::vector< std::array<PMatrix,num_of_transforms> > pmatrices;
   std::vector< std::array<DMatrix,num_of_transforms> > dmatrices;
   //static std::array<RMatrix,SimHash::num_of_levels> rmatrices;
