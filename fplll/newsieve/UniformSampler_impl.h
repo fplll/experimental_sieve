866219f448377e6cda416d71eca4817431550915
kirshewx <elena.kirshanova@rub.de>
2017-12-12 18:28:08 +0100

    UniformSampler

--- fplll/newsieve/UniformSampler_impl.h
+++ fplll/newsieve/UniformSampler_impl.h
@@ -1,6 +1,9 @@
+// clang-format status: NOT OK (reason: templates)
+
+// clang-format off
+
 #ifndef UNIFORM_SAMPLER_IMPL_H
 #define UNIFORM_SAMPLER_IMPL_H
-
 
 #include "DefaultIncludes.h"
 #include "Sampler.h"
@@ -9,79 +12,71 @@
 #include "fplll/gso.h"
 #include "fplll/nr/matrix.h"
 #include "fplll/nr/nr_Z.inl"
-#include <random>
-#include <vector>
-#include <math.h>
 #include "LatticeBases.h"
 
+namespace GaussSieve
+{
 
-namespace GaussSieve {
-  
-  template <class SieveTraits, bool MT, class Engine, class Sseq>
-  void UniformSampler<SieveTraits, MT, Engine, Sseq>::custom_init(SieveLatticeBasis<SieveTraits,MT> const & input_basis)
-  {
-    assert(!initialized);
+template<class SieveTraits, bool MT, class Engine, class Sseq>
+void UniformSampler<SieveTraits,MT,Engine,Sseq>::custom_init(
+    SieveLatticeBasis<SieveTraits,MT> const &input_basis)
+{
+  assert(!initialized);
 #ifndef DEBUG_SIEVE_STANDALONE_SAMPLER
-    assert(sieveptr!=nullptr);
+  assert(sieveptr != nullptr);
 #else
-    assert(sieveptr==nullptr);
+  assert(sieveptr == nullptr);
 #endif
-    
-    if(static_init_plainpoint!=nullptr)
+
+  if (static_init_plainpoint != nullptr)
   {
     assert(false);
   }
-  if(static_init_rettype!=nullptr)
+  if (static_init_rettype != nullptr)
   {
     assert(false);
   }
-    
-    dim           = input_basis.ambient_dimension;
-    lattice_rank  = input_basis.lattice_rank;
-    basis.resize(lattice_rank);
-    
-    static_init_rettype   = new StaticInitializer<RetType>(MaybeFixed<SieveTraits::get_nfixed>{dim});
-    static_init_plainpoint= new StaticInitializer<typename SieveTraits::PlainPoint>(MaybeFixed<SieveTraits::get_nfixed>{dim});
-    
-    
-    //copy the basis
-    for (uint_fast16_t i = 0; i < lattice_rank; ++i)
-    {
-        basis[i] = input_basis.get_basis_vector(i).make_copy();
-    }
-    
-    
-    initialized = true;
-  }
-  
-  template <class SieveTraits, bool MT, class Engine, class Sseq>
-  typename SieveTraits::GaussSampler_ReturnType
-  UniformSampler<SieveTraits, MT, Engine, Sseq>::sample (int const thread)
+
+  dim          = input_basis.ambient_dimension;
+  lattice_rank = input_basis.lattice_rank;
+  basis.resize(lattice_rank);
+
+  static_init_rettype    = new StaticInitializer<RetType>(MaybeFixed<SieveTraits::get_nfixed>{dim});
+  static_init_plainpoint = new StaticInitializer<typename SieveTraits::PlainPoint>(
+      MaybeFixed<SieveTraits::get_nfixed>{dim});
+
+  // copy the basis
+  for (uint_fast16_t i = 0; i < lattice_rank; ++i)
   {
-    assert(initialized);
-#ifdef DEBUG_SIEVE_STANDALONE_SAMPLER
-    assert(sieveptr==nullptr);
-#else
-    assert(sieveptr!=nullptr);
-#endif
-    
-    typename SieveTraits::PlainPoint vec;
-    vec.fill_with_zero();
-    
-    for (unsigned int i=0; i<sparcity; ++i)
-    {
-      vec+=basis[sample_uniform<Engine>(dim-1, engine.rnd())];
-      vec-=basis[sample_uniform<Engine>(dim-1, engine.rnd())];
-    }
-    
-    typename SieveTraits::GaussSampler_ReturnType ret;
-    ret = make_from_any_vector<typename SieveTraits::GaussSampler_ReturnType>(vec, dim);
-    return ret;
-    
+    basis[i] = input_basis.get_basis_vector(i).make_copy();
   }
-  
+  initialized = true;
 }
 
+template<class SieveTraits, bool MT, class Engine, class Sseq>
+typename SieveTraits::GaussSampler_ReturnType
+UniformSampler<SieveTraits,MT,Engine,Sseq>::sample(int const thread)
+{
+  assert(initialized);
+#ifdef DEBUG_SIEVE_STANDALONE_SAMPLER
+  assert(sieveptr == nullptr);
+#else
+  assert(sieveptr != nullptr);
+#endif
 
+  typename SieveTraits::PlainPoint vec;
+  vec.fill_with_zero();
+
+  for (unsigned int i = 0; i < sparcity; ++i)
+  {
+    vec += basis[sample_uniform<Engine>(dim - 1, engine.rnd())];
+    vec -= basis[sample_uniform<Engine>(dim - 1, engine.rnd())];
+  }
+
+  typename SieveTraits::GaussSampler_ReturnType ret;
+  ret = make_from_any_vector<typename SieveTraits::GaussSampler_ReturnType>(vec, dim);
+  return ret;
+}
+}
 
 #endif
