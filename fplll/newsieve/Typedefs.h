f742956ec476cbf2936328f8431b00afe440c2d1
Elena <elena.kirshanova@rub.de>
2017-12-20 08:15:40 +0100

    comparing k=2 and k=3

--- fplll/newsieve/Typedefs.h
+++ fplll/newsieve/Typedefs.h
@@ -113,16 +113,16 @@ public:
   //#endif
 
   // clang-format off
-  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_2sieve_lb = {{47}}; //, 64-18}};
-  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_2sieve_ub = {{128 - 47}}; //+9, 64+18}};
+  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_2sieve_lb = {{64-17}}; //, 64-18}};
+  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_2sieve_ub = {{64+17}}; //+9, 64+18}};
 
   // for 3-sieve: outer loop
-  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_lb_out = {{55}}; //, 64-11}};
-  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_ub_out = {{128 - 55}}; //, 64+11}};
+  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_lb_out = {{64-14}}; //, 64-11}};
+  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_ub_out = {{64+14}}; //, 64+11}};
 
   // for 3-sieve: outer loop
-  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_lb_inn = {{58}}; //, 64-6}};
-  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_ub_inn = {{128 - 58}}; //, 64+6}};
+  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_lb_inn = {{64-10}}; //, 64-6}};
+  constexpr static std::array<unsigned int, sim_hash_num> threshold_lvls_3sieve_ub_inn = {{64+10}}; //, 64+6}};
 
   // for 3-sieve: FilteredList is implemented as vector
   // we reserve filtered_list_size_max inside for its length
